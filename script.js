5283dc208a1c0d002b749ed76165d99309fe342a
Nashra-shakeel <2024cs227@student.uet.edu.pk>
2026-08-19 11:32:26 -0700

    commit

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 let container;
 
-async function loadData(){
-    
-    container = document.querySelector(".container");
+async function loadData() {
+  container = document.querySelector(".container");
   const response = await fetch("data.json");
   if (!response.ok) {
     throw new Error("data not found");
   }
   const jsonData = await response.json();
- let htmlstring="";
-  for( let extention of jsonData){
-    htmlstring+=` <div class="extention border">
+  let htmlstring = "";
+  for (let extention of jsonData) {
+    htmlstring += ` <div class="extention border">
         <div class="main">
           <img
             src="${extention.logo}"
@@ -25,38 +24,40 @@ async function loadData(){
         <button class="remove border">Remove</button>
         <div class="switch-div">
           <label class="switch">
-            <input type="checkbox" ${extention.isActive? "checked": ""} />
+            <input type="checkbox" ${extention.isActive ? "checked" : ""} />
             <span class="slider round"></span>
           </label>
         </div>
       </div>`;
   }
-  container.innerHTML= htmlstring
+  container.innerHTML = htmlstring;
 }
 
+async function statusUpdate(event) {
+  let buttonList = document.querySelector(".buttons");
+  for (const button of buttonList.children) {
+    button.classList.remove("active");
+  }
 
+  let button = event.target;
 
-async function statusUpdate(event){
-    let buttonList=document.querySelector(".buttons");
-    for(const button of buttonList.children)
-    {
-        button.classList.remove("active")
+  button.classList.add("active");
+  for (let extention of container.children) {
+    const input = extention.querySelector("input");
+    if (button.classList.contains("active-button")) {
+      if (input.checked) {
+        extention.style.display = "";
+      } else {
+        extention.style.display = "none";
+      }
+    } else if (button.classList.contains("inactive-button")) {
+      if (!input.checked) {
+        extention.style.display = "";
+      } else {
+        extention.style.display = "none";
+      }
+    } else if (button.classList.contains("active")) {
+      extention.style.display = "";
     }
-
-    let button =event.target;
-    
-    button.classList.add("active");
-    for(let extention of container.children){
-        const input = extention.querySelector("input")
-        if(input.checked){
-            extention.style.display="";
-
-        }
-        else{
-            extention.style.display="none";
-        }
-    }
+  }
 }
-//git init
-// deploy 
-//filter
