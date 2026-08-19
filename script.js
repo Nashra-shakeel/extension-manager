4a287e61f38556c442e19a3e054f4b6c9ab12a5a
Nashra-shakeel <2024cs227@student.uet.edu.pk>
2026-08-19 10:38:35 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+let container;
+
+async function loadData(){
+    
+    container = document.querySelector(".container");
+  const response = await fetch("data.json");
+  if (!response.ok) {
+    throw new Error("data not found");
+  }
+  const jsonData = await response.json();
+ let htmlstring="";
+  for( let extention of jsonData){
+    htmlstring+=` <div class="extention border">
+        <div class="main">
+          <img
+            src="${extention.logo}"
+          />
+          <div class="content">
+            <p class="devlens"><b>${extention.name}</b></p>
+            <p class="para">
+              ${extention.description}
+            </p>
+          </div>
+        </div>
+        <button class="remove border">Remove</button>
+        <div class="switch-div">
+          <label class="switch">
+            <input type="checkbox" ${extention.isActive? "checked": ""} />
+            <span class="slider round"></span>
+          </label>
+        </div>
+      </div>`;
+  }
+  container.innerHTML= htmlstring
+}
+
+
+
+async function statusUpdate(event){
+    let buttonList=document.querySelector(".buttons");
+    for(const button of buttonList.children)
+    {
+        button.classList.remove("active")
+    }
+
+    let button =event.target;
+    
+    button.classList.add("active");
+    for(let extention of container.children){
+        const input = extention.querySelector("input")
+        if(input.checked){
+            extention.style.display="";
+
+        }
+        else{
+            extention.style.display="none";
+        }
+    }
+}
+//git init
+// deploy 
+//filter
