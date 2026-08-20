b8e18b85c4a9345926ab7c3c83466c7bfcd0878d
Nashra-shakeel <2024cs227@student.uet.edu.pk>
2026-08-20 11:21:54 -0700

    commit

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+let toggle = document.querySelector(".sun-div ");
+toggle.addEventListener("click", function () {
+  let body = document.querySelector("body");
+  body.classList.toggle("light");
+  const img = document.querySelector(".image");
+  if (body.classList.contains("light")) {
+    img.src =
+      "/browser-extensions-manager-ui-main//assets/images/icon-moon.svg";
+  } else {
+    toggle.classList.toggle("light");
+    img.src = "/browser-extensions-manager-ui-main//assets/images/icon-sun.svg";
+  }
+});
+let search = document.querySelector(".search");
+let searchDiv = document.querySelector(".search-div");
+search.addEventListener("click", function () {
+  searchDiv.style.display = "";
+});
+
+let input = document.querySelector("input");
+input.addEventListener("input", function () {
+  searchByDescription(input.value);
+});
 let container;
 
+function searchByDescription(description) {
+  for (let extention of container.children) {
+    const para = extention.querySelector(".para");
+      if (para.textContent.includes(description)) {
+        extention.style.display = "";
+      } else {
+        extention.style.display = "none";
+      }
+  }
+}
+
 async function loadData() {
+  searchDiv.style.display = "none";
   container = document.querySelector(".container");
   const response = await fetch("data.json");
   if (!response.ok) {
@@ -38,25 +73,28 @@ async function statusUpdate(event) {
   for (const button of buttonList.children) {
     button.classList.remove("active");
   }
-
   let button = event.target;
 
   button.classList.add("active");
+
   for (let extention of container.children) {
     const input = extention.querySelector("input");
     if (button.classList.contains("active-button")) {
+      searchDiv.style.display = "none";
       if (input.checked) {
         extention.style.display = "";
       } else {
         extention.style.display = "none";
       }
     } else if (button.classList.contains("inactive-button")) {
+      searchDiv.style.display = "none";
       if (!input.checked) {
         extention.style.display = "";
       } else {
         extention.style.display = "none";
       }
-    } else if (button.classList.contains("active")) {
+    } else {
+      searchDiv.style.display = "none";
       extention.style.display = "";
     }
   }
